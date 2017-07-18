8075be953dc3a4a61b17d7831c318496d8778cc6
Jeikosd <jeison.mesa@correounivalle.edu.co>
2017-07-18 11:41:58 -0500

    start project sensi analysis

---
+++ data/Experiment_data/000004/000004.bat
@@ -0,0 +1,8 @@
+C:\DSSAT46\dscsm046 CRGRO046 B D:\B\0\000004\batch000004.v46
+rename PlantGro.OUT summary.txt
+rename Overview.OUT overview.txt
+del *.OUT
+rename summary.txt PlantGro.OUT
+rename overview.txt Overview.OUT
+@echo off
+exit
