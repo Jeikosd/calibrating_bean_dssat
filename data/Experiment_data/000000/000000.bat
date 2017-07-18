8d9ddb974b014398779db803ad6e92c18d6d9925
Jeikosd <jeison.mesa@correounivalle.edu.co>
2017-07-18 13:54:57 -0500

    fix real x-file

--- data/Experiment_data/000000/000000.bat
+++
@@ -1,8 +0,0 @@
-C:\DSSAT46\dscsm046 CRGRO046 B D:\B\0\000000\batch000000.v46
-rename PlantGro.OUT summary.txt
-rename Overview.OUT overview.txt
-del *.OUT
-rename summary.txt PlantGro.OUT
-rename overview.txt Overview.OUT
-@echo off
-exit
--- data/Experiment_data/000007/000007.bat
+++
@@ -1,8 +0,0 @@
-C:\DSSAT46\dscsm046 CRGRO046 B D:\B\0\000007\batch000007.v46
-rename PlantGro.OUT summary.txt
-rename Overview.OUT overview.txt
-del *.OUT
-rename summary.txt PlantGro.OUT
-rename overview.txt Overview.OUT
-@echo off
-exit
