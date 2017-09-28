1a4a97c8f062f29c652b5c66d7b83619d51a3fb1
Jeikosd <jeison.mesa@correounivalle.edu.co>
2017-09-28 10:08:50 -0500

    add functions that make random numbers loading a matrix of parameters and then a function that write the information in a file .CUL

--- make_cultivar.R
+++ make_cultivar.R
@@ -6,7 +6,7 @@ library(tidyverse)
 library(stringr)
 library(magrittr)
 
-file <- 'D:/CIAT/Ayudas_2017/Patricia Alvarez/DSSAT_bean/data/Experiment_data/Barichara/BNGRO046.CUL'
+file <- 'data/Experiment_data/Barichara/BNGRO046.CUL'
 cultivar <- 'IB0035'
 
 ## sel_cul(file, cultivar)
@@ -34,11 +34,31 @@ sel_cul <- function(file, cultivar){
   
 }
 
-
+make_cul_df <- function(cul_df, default_variables, vars_df){
+  
+  # cul_df
+  # default_variables <- varaiables_static
+  # vars_df <- variables_to_change
+  
+  order_cul <- colnames(cul_df)
+  
+  cul_df <- cul_df %>%
+    select(!!default_variables) %>%
+    cbind(vars_df) %>% 
+    tbl_df() %>% 
+    select(!!order_cul)
+  
+  return(cul_df)
+}
 ## 
 
 # inputs_df <- read_csv(paste0('data/rangos_coeficientes.csv'))
-make_combination <- function(file, inputs_df, cultivar){
+# k cantidad de numeros aleatorios
+
+
+
+
+make_combination <- function(file, inputs_df, cultivar, k){
   
   # This are the parameters @VAR#  VRNAME.......... EXPNO   ECO#  CSDL PPSEN EM-FL FL-SH FL-SD SD-PM FL-LF LFMAX SLAVR SIZLF  XFRT WTPSD SFDUR SDPDV PODUR THRSH SDPRO SDLIP
   
@@ -63,25 +83,57 @@ make_combination <- function(file, inputs_df, cultivar){
     cul_df <- read_fwf(file, fwf_widths(widths_cul, col_names = header_cul), skip = find_cul, n_max = 1, col_types = cols())
     
     # match between the variables that we have in the inputs to the .CUL file  
-    ## pensar como hacer el match para variar los parametros que se indican en el input file
     
-  proof <- inputs_df %>%
-    magrittr::extract2(1) 
+    variables_to_run <- inputs_df %>%
+      magrittr::extract2(1)
   
-  header_cul %>%
-    data_frame(coefficients = .) %>%
-    filter(coefficients %in% proof)
-  %>%
-    str_match(header_cul, .)
-    coefficients, header_cul
+    varaiables_static <- header_cul %>%
+      data_frame(coefficients = .) %>%
+      filter(!str_detect(coefficients,  paste(variables_to_run, collapse = '|'))) %>%
+      magrittr::extract2(1)
+    
+    
+    ## this make the random numbers following a uniform distribution
+    variables_to_change  <- inputs_df %>%
+      mutate(data = map2(min, max, runif, n = k)) %>%
+      select(coefficients, data) %>%
+      unnest() %>%
+      group_by(coefficients) %>%
+      mutate(id = 1:length(coefficients)) %>%
+      spread(coefficients, data) %>%
+      select(-id)
+      
+    
+    
+    random_cul <- make_cul_df(cul_df, varaiables_static, variables_to_change)
     
   
-  header_cul
-  
-  str_match()
-  
+    
+    return(random_cul)
+    
+    
+    # inputs_df %>%
+    #   mutate(data = pmap(list(min, max, by), seq)) %>%
+    #   select(coefficients, data) %>%
+    #   unnest() %>%
+    #   group_by(coefficients) %>%
+    #   mutate(id = 1:length(coefficients)) %>%
+    #   ungroup() %>%
+    #   spread(coefficients, data) %>%
+    #   select(-id) %>%
+    #   filter(row_number()<=8) %>%
+    #   expand(!!!grouping) 
+
+  # proof <- paste(variables_to_run, collapse = ',')
+  # grouping <- rlang::syms(as.list(variables_to_run))
 }
 
+
+make_combination(file = 'data/Experiment_data/Barichara/BNGRO046.CUL',
+                 inputs_df = read_csv(paste0('data/rangos_coeficientes.csv')), 
+                 cultivar = 'IB0035', 
+                 k = 1) 
+## funcion para escribir el anterior cultivar en el archivo *.CUL
 
 
 
---
+++ write_cul.R
@@ -0,0 +1,71 @@
+## write BNGRO046.CUL
+
+
+x <- make_combination(file = 'data/Experiment_data/Barichara/BNGRO046.CUL',
+                 inputs_df = read_csv(paste0('data/rangos_coeficientes.csv')), 
+                 cultivar = 'IB0035', 
+                 k = 1) 
+
+write_cul <- function(matrix_cul, out_dir){
+  
+  # matrix_cul <- x
+  
+  ## make the variables to type in the .CUL
+  cultivar_id <- magrittr::extract2(matrix_cul, 1)
+  cultivar_name <- magrittr::extract2(matrix_cul, 2)
+  ecotype <- magrittr::extract2(matrix_cul, 4)
+  CSDL <- magrittr::extract2(matrix_cul, 5)
+  PPSEN <- magrittr::extract2(matrix_cul, 6)
+  EM_FL <- magrittr::extract2(matrix_cul, 7)
+  FL_SH <- magrittr::extract2(matrix_cul, 8)
+  FL_SD <- magrittr::extract2(matrix_cul, 9)
+  SD_PM <- magrittr::extract2(matrix_cul, 10)
+  FL_LF <- magrittr::extract2(matrix_cul, 11)
+  LFMAX <- magrittr::extract2(matrix_cul, 12)
+  SLAVR <- magrittr::extract2(matrix_cul, 13)
+  SIZLF <- magrittr::extract2(matrix_cul, 14)
+  XFRT <- magrittr::extract2(matrix_cul, 15)
+  WTPSD <- magrittr::extract2(matrix_cul, 16)
+  SFDUR <- magrittr::extract2(matrix_cul, 17)
+  SDPDV <- magrittr::extract2(matrix_cul, 18)
+  PODUR <- magrittr::extract2(matrix_cul, 19)
+  THRSH <- magrittr::extract2(matrix_cul, 20)
+  SDPRO <- magrittr::extract2(matrix_cul, 21)
+  SDLIP <- magrittr::extract2(matrix_cul, 22)
+  
+  
+  sink(paste0(out_dir, 'BNGRO046.CUL'), append = F)
+  
+  cat("@VAR#  VRNAME.......... EXPNO   ECO#  CSDL PPSEN EM-FL FL-SH FL-SD SD-PM FL-LF LFMAX SLAVR SIZLF  XFRT WTPSD SFDUR SDPDV PODUR THRSH SDPRO SDLIP")
+  # 
+  cat("\n")
+  cat(paste(sprintf("%6s", cultivar_id),
+            sprintf("%-16s", cultivar_name), 
+            sprintf("%5s", '.'),
+            sprintf("%6s", ecotype),
+            sprintf("%5.2f", CSDL),
+            sprintf("%5.3f", PPSEN),
+            sprintf("%5.1f", EM_FL),
+            sprintf("%5.1f", FL_SH),
+            sprintf("%5.1f", FL_SD), 
+            sprintf("%5.2f", SD_PM),
+            sprintf("%5.2f", FL_LF),
+            sprintf(" %04.2f", LFMAX),
+            sprintf("%4i.", as.integer(SLAVR)),
+            sprintf("%5.1f", SIZLF),
+            sprintf(" %4.2f", XFRT),
+            sprintf("%05.3f", WTPSD),
+            sprintf(" %4.1f", SFDUR), 
+            sprintf(" %4.2f", SDPDV),
+            sprintf(" %4.1f", PODUR),
+            sprintf(" %4.1f", THRSH),
+            "", sub("^(-?)0.", "\\1.", sprintf("%.3f", SDPRO)),
+            "", sub("^(-?)0.", "\\1.", sprintf("%.3f", SDLIP))
+  ))
+  sink()
+}
+
+write_cul(x, 
+          out_dir = 'Runs/')
+
+## CRGRO046 Modelo de Frijol utilizado 
